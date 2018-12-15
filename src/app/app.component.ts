7b0eea8a6d4404cb8b80d102fe71c79deb950d6b
peedekza40 <45896354+peedekza40@users.noreply.github.com>
2018-12-16 01:07:20 +0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { ReportPage } from '../pages/report/report';
 import { TmanagePage } from '../pages/tmanage/tmanage';
-=======
 import { ReportPage } from '../pages/report/report'; 
->>>>>>> 0986bf227a29b34767982837370287cb5a462503
-=======
-import { ReportPage } from '../pages/report/report';  
->>>>>>> 24b66611efb45d94a7ef1ae787f217a3e47add63
 
 @Component({
   templateUrl: 'app.html'
@@ -33,12 +26,9 @@ export class MyApp {
     this.pages = [
       { title: 'Home', component: HomePage },
       { title: 'List', component: ListPage },
-<<<<<<< HEAD
       { title: 'Report', component: ReportPage  },
-      { title: 'TmanagePage', component: TmanagePage  }
-=======
+      { title: 'TmanagePage', component: TmanagePage  },
       { title: 'Report', component: ReportPage },
->>>>>>> 0986bf227a29b34767982837370287cb5a462503
     ];
 
   }

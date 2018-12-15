dbe60663a9de4810b44e727274274cea8cf69392
Ruangwit Nukboon <59160214@go.buu.ac.th>
2018-12-15 19:30:42 +0700

    Enter

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { hasLifecycleHook } from '@angular/compiler/src/lifecycle_reflector';
 
 @Component({
   selector: 'page-home',
@@ -12,6 +13,6 @@ export class HomePage {
   }
 
   somefunction(){
-    
+    sdasdasdasd
   }
 }

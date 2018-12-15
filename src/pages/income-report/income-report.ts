d79e98865a9986f6e15f354f6ed6090572ff6dce
Spy59160152 <45897284+Spy59160152@users.noreply.github.com>
2018-12-16 06:19:50 +0700

    report income calculate success

--- src/pages/income-report/income-report.ts
+++ src/pages/income-report/income-report.ts
@@ -14,12 +14,65 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'income-report.html',
 })
 export class IncomeReportPage {
+  details : Array <any> = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    this.details = [
+                    {
+                      "code" : "RE61100021",
+                      "date" : "01-10-2561",
+                      "type" : "income",
+                      "amount" : "250"
+                    },
+                    {
+                      "code" : "RE61100022",
+                      "date" : "01-10-2561",
+                      "type" : "income",
+                      "amount" : "120"
+                    },
+                    {
+                      "code" : "RE61100023",
+                      "date" : "01-10-2561",
+                      "type" : "income",
+                      "amount" : "400"
+                    },
+                    {
+                      "code" : "RE61100024",
+                      "date" : "01-10-2561",
+                      "type" : "income",
+                      "amount" : "190"
+                    },
+                    {
+                      "code" : "RE61100025",
+                      "date" : "01-10-2561",
+                      "type" : "income",
+                      "amount" : "30"
+                    },
+                    {
+                      "code" : "RE61100026",
+                      "date" : "01-10-2561",
+                      "type" : "income",
+                      "amount" : "200"
+                    }
+                  ]
+
+                } 
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad IncomeReportPage');
   }
+
+  get_sum(){
+    var sum_total = 0;
+    var y =0;
+    for (let i=0; i<this.details.length; i++) {
+      y = +(this.details[i].amount);
+      sum_total += y;
+    }
+      console.log(this.details[0].amount);
+      return sum_total;
+      
+  }
+  
 
 }

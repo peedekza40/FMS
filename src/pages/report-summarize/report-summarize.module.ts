80e23d7f561ff2c8b9ce609c25d4373417ac698a
59160215 <59160215@go.buu.ac.th>
2018-12-15 22:07:35 +0700

    enter

---
+++ src/pages/report-summarize/report-summarize.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ReportSummarizePage } from './report-summarize';
+
+@NgModule({
+  declarations: [
+    ReportSummarizePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ReportSummarizePage),
+  ],
+})
+export class ReportSummarizePageModule {}

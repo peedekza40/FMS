f9f3a2f581a8ddf462c62f14904a2223ce98bb7c
59160215 <59160215@go.buu.ac.th>
2018-12-17 01:06:23 +0700

    a commit

--- src/pages/summarize-report/summarize-report.ts
+++ src/pages/summarize-report/summarize-report.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { NgForOf } from '@angular/common';
 import { defaultIterableDiffers } from '@angular/core/src/change_detection/change_detection';
-
+import { AlertController } from 'ionic-angular';
 /**
  * Generated class for the SummarizeReportPage page.
  *
@@ -19,61 +19,61 @@ export class SummarizeReportPage {
 
   details : Array <any> = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
     this.details = [
                     {
                       "code" : "RE61100021",
                       "date" : "01-10-2561",
                       "type" : "income",
-                      "amount" : "250"
+                      "amount" : "250.00"
                     },
                     {
                       "code" : "RE61100022",
                       "date" : "01-10-2561",
                       "type" : "income",
-                      "amount" : "120"
+                      "amount" : "120.00"
                     },
                     {
                       "code" : "PA61100022",
                       "date" : "01-10-2561",
                       "type" : "payment",
-                      "amount" : "500"
+                      "amount" : "500.00"
                     },
                     {
                       "code" : "RE61100023",
                       "date" : "01-10-2561",
                       "type" : "income",
-                      "amount" : "400"
+                      "amount" : "400.00"
                     },
                     {
                       "code" : "PA61100020",
                       "date" : "01-10-2561",
                       "type" : "payment",
-                      "amount" : "140"
+                      "amount" : "140.00"
                     },
                     {
                       "code" : "RE61100024",
                       "date" : "01-10-2561",
                       "type" : "income",
-                      "amount" : "190"
+                      "amount" : "190.00"
                     },
                     {
                       "code" : "PA61100021",
                       "date" : "01-10-2561",
                       "type" : "payment",
-                      "amount" : "210"
+                      "amount" : "210.00"
                     },
                     {
                       "code" : "RE61100025",
                       "date" : "01-10-2561",
                       "type" : "income",
-                      "amount" : "30"
+                      "amount" : "30.00"
                     },
                     {
                       "code" : "RE61100026",
                       "date" : "01-10-2561",
                       "type" : "income",
-                      "amount" : "200"
+                      "amount" : "200.00"
                     }
                   ]
 
@@ -95,8 +95,37 @@ export class SummarizeReportPage {
         sum_total -= y;
       }
     }
-      return sum_total;
-      
+      return sum_total.toFixed(2);
   }
 
+  show_alert_del(detail){
+    const confirm = this.alertCtrl.create({
+      title: 'ลบรายการบัญชี',
+      message: 'ต้องการลบรายการบัญชี?',
+      buttons: [
+        {
+          text: 'ยกเลิก',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'ยืนยัน',
+          handler: () => {
+            this.details.forEach((element, index) => {
+              if (element == detail) {
+              this.details.splice(index,1);
+              }
+            });
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+  show_info(){
+
+  }
 }

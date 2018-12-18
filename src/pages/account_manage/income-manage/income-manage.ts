ddc236be7ccf762aacafa2b9b29e3d94f4261a50
59160437 <59160437@go.buu.ac.th>
2018-12-19 06:08:39 +0700

    update

--- src/pages/account_manage/income-manage/income-manage.ts
+++ src/pages/account_manage/income-manage/income-manage.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController, ViewController, Refresher  } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController, ViewController} from 'ionic-angular';
 import { MReportIncomeProvider } from '../../../providers/m-report-income/m-report-income';
 import * as moment from 'moment';
 
@@ -26,7 +26,7 @@ export class IncomeManagePage implements OnInit {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad IncomeManagePage');
+    //console.log('ionViewDidLoad IncomeManagePage');
   }
   ngOnInit() {
     this.MReportIncomeProvider.get_by_inc_date().subscribe((response) => {
@@ -43,35 +43,33 @@ export class IncomeManagePage implements OnInit {
   }
 
   openEditModal(id){
-    // let para_data = {
-    //   inc_id:id
-    // };
     const myModal = this.editModal.create('EditModalPage', {inc_id:id});
     myModal.present();
 
     myModal.onWillDismiss((data) => {
       //console.log(data);
-      
-      var inc_id = data[0].inc_id;
-      var inc_date = moment(data[0].inc_date).format("YYYY-MM-DD");
-      var inc_receipt = data[0].inc_receipt;
-      var inc_receipt_code = data[0].inc_receipt_code;
-      var inc_receipt_date = moment(data[0].inc_receipt_date).format("YYYY-MM-DD");
-      var inc_amount = data[0].inc_amount;
-      var inc_detail = data[0].inc_detail;
-      var inc_desc_id = data[0].inc_desc_id;
-      var inc_bac_id = data[0].inc_bac_id; 
-      var inc_status = data[0].inc_status;
-      var inc_editor = data[0].inc_editor;
+      if(data){
+        var inc_id = data[0].inc_id;
+        var inc_date = moment(data[0].inc_date).format("YYYY-MM-DD");
+        var inc_receipt = data[0].inc_receipt;
+        var inc_receipt_code = data[0].inc_receipt_code;
+        var inc_receipt_date = moment(data[0].inc_receipt_date).format("YYYY-MM-DD");
+        var inc_amount = data[0].inc_amount;
+        var inc_detail = data[0].inc_detail;
+        var inc_desc_id = data[0].inc_desc_id;
+        var inc_bac_id = data[0].inc_bac_id; 
+        var inc_status = data[0].inc_status;
+        var inc_editor = data[0].inc_editor;
 
-      this.MReportIncomeProvider.update_income(inc_id, inc_date, inc_receipt, inc_receipt_code, inc_receipt_date, 
-        inc_amount, inc_detail, inc_desc_id, inc_bac_id, inc_status, inc_editor).subscribe((response) => {
-        console.log(response);
-      });
+        this.MReportIncomeProvider.update_income(inc_id, inc_date, inc_receipt, inc_receipt_code, inc_receipt_date, 
+          inc_amount, inc_detail, inc_desc_id, inc_bac_id, inc_status, inc_editor).subscribe((response) => {
+          //console.log(response);
+        });
+      }
     });
 
     myModal.onDidDismiss(() => {
-      location.reload();
+      this.navCtrl.setRoot(this.navCtrl.getActive().component);
     });
   }
 

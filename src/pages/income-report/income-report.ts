d92f48389a62aa66617d53a0b6dcd72d9ecfd396
Spy59160152 <45897284+Spy59160152@users.noreply.github.com>
2018-12-19 19:31:07 +0700

    add function show alert delete

--- src/pages/income-report/income-report.ts
+++ src/pages/income-report/income-report.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { MReportIncomeProvider } from '../../providers/m-report-income/m-report-income';
+import { AlertController } from 'ionic-angular';
 import * as moment from 'moment';
 
 /**
@@ -16,57 +17,74 @@ import * as moment from 'moment';
   templateUrl: 'income-report.html',
 })
 export class IncomeReportPage implements OnInit{
-  report_income:Report_income[];
+  public report_income:Report_income[];
+  
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public MReportIncomeProvider: MReportIncomeProvider) {
-  } 
+  constructor(public navCtrl: NavController, public navParams: NavParams, public MReportIncomeProvider: MReportIncomeProvider , public  alertCtrl: AlertController) {
+  } //constructor
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad IncomeReportPage');
-  }
+  }//ionViewDidLoad
 
   ngOnInit() {
     this.MReportIncomeProvider.get_by_inc_date().subscribe((response) => {
       this.report_income = response;
       let i:number = 0;
       this.report_income.forEach((element) => {
-        console.log(element.inc_amount);
+        //console.log(element.inc_amount);
         i += element.inc_amount;
       });
-      console.log('Sum total : '+i);
+      //console.log('Sum total : '+i);
       //console.log(this.report_income.keys(response).length); 
     });
-  }
+  }//ngOnInit
 
   convert_date(date:string){
     return moment(date).format('DD/MM/YYYY');
-  }
-  /*get_sum(){
-    var sum_total = 0;
-    var y =0;
-    for (let i=0; i<this.report_income.length; i++) {
-      y = +(this.report_income[i].inc_amount);
-      sum_total += y;
-    }
-      console.log(this.report_income[0].inc_amount);
-      return sum_total;
-      
-  }*/
-  /*get_sum(){
-    var sum_total = 0;
-    var y =0;
-    for (let i=0; i<this.report_income.length; i++) {
-        y = +(this.report_income[i].inc_amount);
-        sum_total += y;
-    }
-    return sum_total.toFixed(2);  
-  }*/
+  }//convert_date
 
-}
+  get_sum(){
+    var sum_total = 0;
+    this.report_income.forEach( (element)=> {
+      sum_total += element.inc_amount;
+    });
+      console.log(sum_total);
+      return sum_total;
+  }//get_sum
+
+  show_alert_del(obj){
+    const confirm = this.alertCtrl.create({
+      title: 'ลบรายการบัญชี',
+      message: 'ต้องการลบรายการบัญชี?',
+      buttons: [
+        {
+          text: 'ยกเลิก',
+          handler: () => {
+            console.log('Disagree clicked');
+          }//handler
+        },
+        {
+          text: 'ยืนยัน',
+          handler: () => {
+            this.report_income.forEach((element, index) => {
+              if (element == obj) {
+                this.report_income.splice(index,1);
+              }//if
+            });
+            console.log('Agree clicked');
+          }//handler
+        }
+      ]
+    });
+    confirm.present();
+  }//show_alert_del
+}//export class
+
 interface Report_income{
   bacName: string;
   inc_date: Date;
   inc_code : string;
   desc_description: string;
   inc_amount: number;
-}
+}//interface

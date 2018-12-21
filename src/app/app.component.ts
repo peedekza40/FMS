7c979ee6957d6d15ce907ddb9cd853db248260c6
“peedekza40” <59160214@go.buu.ac.th>
2018-12-21 21:37:14 +0700

    update icon app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,8 +32,9 @@ export class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      this.statusBar.styleLightContent();
+      this.splashScreen.show();
+      this.statusBar.show();
     });
   }
 
